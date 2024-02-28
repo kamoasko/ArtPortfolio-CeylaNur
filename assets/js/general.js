5ca9a4192fecd4783b955bd255b386adfd465246
kamoasko <kamdesigning@gmail.com>
2024-02-28 23:11:59 +0400

    header done

--- assets/js/general.js
+++ assets/js/general.js
@@ -1,1 +1,24 @@
 "use strict";
+
+// hamburger menu tablet
+
+let hamburgerBtn = document.querySelector(".plate");
+let pageTopNav = document.querySelector(".header__top-nav");
+let windowWidth = window.innerWidth;
+let navbar = document.querySelector(".navbar");
+
+const responsiveHamburger = () => {
+  if (windowWidth <= 768 && windowWidth > 500) {
+    hamburgerBtn.addEventListener("click", function () {
+      this.classList.toggle("active");
+      pageTopNav.classList.toggle("active");
+    });
+  } else if (windowWidth <= 500) {
+    hamburgerBtn.addEventListener("click", function () {
+      this.classList.toggle("active");
+      navbar.classList.toggle("active");
+    });
+  }
+};
+
+responsiveHamburger();

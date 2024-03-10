d2be3a2dc76596bb60627fedbc97f906996f0ca2
kamoasko <kamdesigning@gmail.com>
2024-03-10 22:56:23 +0400

    homepage finished

--- assets/js/general.js
+++ assets/js/general.js
@@ -22,3 +22,18 @@ const responsiveHamburger = () => {
 };
 
 responsiveHamburger();
+
+// modal
+
+let backdrop = document.querySelector(".backdrop");
+let closeModalBtn = document.querySelector("#modal_close");
+
+function openModal() {
+  backdrop.classList.toggle("active");
+}
+
+openModal();
+
+closeModalBtn.addEventListener("click", () => {
+  backdrop.classList.toggle("active");
+});
